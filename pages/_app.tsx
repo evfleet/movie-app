46f1e7435d804e32a5b8f7e47564b10981614916
evfleet <evfleet@gmail.com>
2020-02-09 15:07:55 -0500

    feat(client): setup theming for styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import theme from "../styles/theme";
 import "../styles/index.scss";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
