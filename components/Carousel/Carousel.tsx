628db61dc90d709867d4c842cbf45956742e7bb9
evfleet <evfleet@gmail.com>
2020-02-09 20:31:04 -0500

    refactor(client): rename carousel component

---
+++ components/Carousel/Carousel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Carousel: React.FunctionComponent = () => {
+  return (
+    <div>
+      <p>Carousel</p>
+    </div>
+  );
+};
+
+export default Carousel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import HeroCarousel from "../components/HeroCarousel";
+import Carousel from "../components/Carousel";
 
 const IndexPage = () => (
   <Layout container={true}>
     <p>Hello from Movies 123</p>
-    <HeroCarousel />
+    <Carousel />
   </Layout>
 );
 
