7b20971b259c3bcf46295d89be34b7bc76b5e188
evfleet <evfleet@gmail.com>
2020-02-11 11:05:33 -0500

    feat(client): implement bulma navbar

--- components/NavBar/styled.tsx
+++ components/NavBar/styled.tsx
@@ -2,6 +2,8 @@ import styled from "../../styles/themed-styled";
 
 import { Wrapper } from "../../shared/styled";
 
-const NavWrapper = styled(Wrapper)``;
+const NavWrapper = styled(Wrapper)`
+  background-color: ${({ theme }) => theme.colours.darkBlue};
+`;
 
 export { NavWrapper };
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,17 @@
-export interface Theme {}
+export interface Theme {
+  colours: {
+    [key: string]: string;
+  };
+}
 
-const theme: Theme = {};
+const theme: Theme = {
+  colours: {
+    darkBlue: "#0A2342",
+    mediumBlue: "#2C63A5",
+    greyBlue: "#849DBC",
+    offWhite: "#F7FAFF",
+    accentBlue: "#61A4F4"
+  }
+};
 
 export default theme;
