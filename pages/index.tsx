99085f03cd7dc5cab12fdf1a83a75354e0119666
evfleet <evfleet@gmail.com>
2020-02-12 08:59:59 -0500

    feat(client): data fetching for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import fetch from "isomorphic-unfetch";
 
 import Layout from "../components/Layout";
 import Carousel from "../components/Carousel";
 import { Container, Wrapper } from "../shared/styled";
 
-const IndexPage = () => (
-  <Layout>
-    <Wrapper>
-      <Container>
-        <Carousel />
-      </Container>
-    </Wrapper>
-  </Layout>
-);
+const IndexPage = ({ results }) => {
+  return (
+    <Layout>
+      <Wrapper>
+        <Container>
+          <Carousel />
+        </Container>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+IndexPage.getInitialProps = async () => {
+  const results = await fetch(
+    "http://localhost:3000/api/movies/current"
+  ).then(r => r.json());
+
+  return {
+    results
+  };
+};
 
 export default IndexPage;
